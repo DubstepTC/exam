637c7f7bbe03b32b11e40a6555e9ae77c814b608
DubstepTC <118935884+DubstepTC@users.noreply.github.com>
2023-11-26 01:28:07 +0300

    Create main.dart

---
+++ exam01.03_ex17/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Настройки'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Личный кабинет',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Имя',
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Фамилия',
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Адрес электронной почты',
+              ),
+            ),
+            // Другие поля настроек...
+
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Логика сохранения настроек
+              },
+              child: Text('Сохранить'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
