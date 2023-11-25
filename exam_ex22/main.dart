1198a6bd0a7b5dbe0d11d4b51ffa2986ad0daa5d
DubstepTC <118935884+DubstepTC@users.noreply.github.com>
2023-11-26 01:38:14 +0300

    Create main.dart

---
+++ exam_ex22/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'apartment_details_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Apartment Details',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ApartmentDetailsScreen(), // Этот виджет будет запущен первым
+    );
+  }
+}
