f0883c805f0a72fcb7c6cd1fd7d8e25c948b4def
DubstepTC <118935884+DubstepTC@users.noreply.github.com>
2023-11-26 16:53:57 +0300

    Update main.dart

--- exam01.03_ex2/main.dart
+++ exam01.03_ex2/main.dart
@@ -29,15 +29,34 @@ class _CalculatorState extends State<Calculator> {
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children:
+          <Widget>[
+            Container(
+              width: 300, // Задаем ширину контейнера
+              height: 50, // Задаем высоту контейнера
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple), // Устанавливаем границу
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none, // Убираем внутренние границы текстового поля
+                  contentPadding: EdgeInsets.all(10), // Задаем отступ внутри поля
+                ),
+              ),
+            ),
+            const SizedBox(height: 60,),
             // Ряд 1
             buildButtonRow(['7', '8', '9', '/']),
+            const SizedBox(height: 20,),
             // Ряд 2
             buildButtonRow(['4', '5', '6', 'x']),
+            const SizedBox(height: 20,),
             // Ряд 3
             buildButtonRow(['1', '2', '3', '-']),
+            const SizedBox(height: 20,),
             // Ряд 4
             buildButtonRow(['C', '0', '=', '+']),
+            const SizedBox(height: 20,),
           ],
         ),
       ),
@@ -48,8 +67,7 @@ class _CalculatorState extends State<Calculator> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: texts
-          .map((text) => FlatButton(
-                padding: EdgeInsets.all(16.0),
+          .map((text) => TextButton(
                 onPressed: () {
                   // Действия при нажатии кнопки
                 },
