3b5ad6c062b0a1c4570cbdb8d50324801a438aa5
0lexander1 <134669779+0lexander1@users.noreply.github.com>
2023-11-26 21:41:43 +0300

    Update main.dart

--- exam01.03_ex15/main.dart
+++ exam01.03_ex15/main.dart
@@ -15,9 +15,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Product {
+  final String name;
+  final String imageUrl;
+  final String description;
+  final double price;
+
+  Product({
+    required this.name,
+    required this.imageUrl,
+    required this.description,
+    required this.price,
+  });
+}
+
 class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Product product = Product(
+      name: "Платье",
+      imageUrl: "assets/kat.jpg",
+      description: "Красивое платье для особых случаев",
+      price: 99.99,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Информация о продукте'),
@@ -28,8 +49,8 @@ class ProductScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Center(
-              child: Image.network(
-                'https://example.com/product-image.jpg',
+              child: Image.asset(
+                product.imageUrl, // использование локальной картинки из assets
                 width: 200,
                 height: 200,
                 fit: BoxFit.cover,
@@ -37,14 +58,14 @@ class ProductScreen extends StatelessWidget {
             ),
             SizedBox(height: 16.0),
             Text(
-              'Название продукта',
+              product.name,
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Text(
-              'Цена: \$99.99',
+              'Цена: \$${product.price.toStringAsFixed(2)}', // форматирование до двух знаков после запятой
               style: TextStyle(
                 fontSize: 18,
                 color: Colors.black87,
@@ -52,7 +73,7 @@ class ProductScreen extends StatelessWidget {
             ),
             SizedBox(height: 8.0),
             Text(
-              'Описание продукта...',
+              product.description,
               style: TextStyle(
                 fontSize: 16,
               ),
